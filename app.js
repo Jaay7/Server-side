3b0b7953aae1b5554a2a3de7242ceca7828ef772
Jaay7 <jayakrishna.madem@gmail.com>
2020-11-08 23:30:52 +0530

    second commit

--- app.js
+++ app.js
@@ -6,32 +6,29 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-//mongodb connection
-mongoose.connect(config.database);
-
-mongoose.connection.on('connected', () => {
-    console.log('connected to database ' + config.database);
-});
-
-mongoose.connection.on('error', (err) => {
-    console.log('database error ' + err);
-});
+// Connect To Database
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.database, { useMongoClient: true, promiseLibrary: require('bluebird') })
+  .then(() => console.log(`Connected to database ${config.database}`))
+  .catch((err) => console.log(`Database error: ${err}`));
 
 const app = express();
 
 const users = require('./routes/users');
 
+// Port Number
 const port = 3000;
 
+// CORS Middleware
 app.use(cors());
 
-// SET STATIC FOLDER
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body Parser 
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-// passport
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -39,12 +36,12 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-// index route
+// Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid endpoint');
+  res.send('Invalid Endpoint');
 });
 
-// start server
+// Start Server
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
+  console.log('Server started on port '+port);
 });
